d0fc6179f91362f077cd25c4d5ab3efc8493c1bb
nicholassan67 <nicholassan67@gmail.com>
2022-07-15 20:06:58 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assign_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 5;
+            int[] boo = new int[] { 0, 1, 2, 3, 4 };
+            int m = 100;
+            int[] scar = new int[100];
+            for (int i = 0; i < m; i++)
+            {
+                scar[i] = i;
+            }
+            Console.WriteLine($"The total number of even numbers in boo are: {countEvenNum(boo, n)}");
+            Console.WriteLine($"The total number of even numbers in scar are: {countEvenNum(scar, m)}");
+        }
+
+        public static int countEvenNum(int[] arr, int size)
+        {
+            int count = 0;
+            for (int i = 0; i < size; i++)
+            {
+                if (arr[i] % 2 == 0)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
+
+
